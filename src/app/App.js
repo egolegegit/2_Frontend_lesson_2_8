dd96a93c2016dbaaf3c3469340467d1025818212
Рыбников Андрей Анатольевич <43681241+egolegegit4226@users.noreply.github.com>
2021-09-29 01:07:36 +0700

    fix package.json

--- src/app/App.js
+++ src/app/App.js
@@ -10,12 +10,10 @@ import NotFound from './components/not-found'
 
 function App() {
   return (
-    <div className="flex flex-col w-full h-screen p-6 bg-gray-200 dark:bg-gray-700 dark:text-white">
+    <div className="flex flex-col w-full h-screen p-6 bg-gray-200 dark:bg-gray-700 ">
       <NavBar />
 
-      <h1 className="flex justify-center py-8 text-4xl font-medium dark:text-white">
-        App
-      </h1>
+      <h1 className="flex justify-center py-8 text-4xl font-medium">App</h1>
 
       <Switch>
         <Route exact path="/" component={Home} />
